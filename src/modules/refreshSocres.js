298dca6de1aa0bea5cff97b9e4bca8907abb12fa
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-06-29 19:04:49 +0300

    fix linter error

--- src/modules/refreshSocres.js
+++ src/modules/refreshSocres.js
@@ -1,32 +1,49 @@
-import constants from "./env.js";
+import constants from './env.js;
 
 const { BASE_URL, GAME_ID } = constants();
 
 const pageSize = 5; // Number of scores to display per page
 let currentPage = 1; // Current page number
+let sortByName = ''; // Sorting order for name (empty for default)
+let sortByScore = ''; // Sorting order for score (empty for default)
 
 let scoresData = []; // Array to store all scores data
 
 const refreshScores = () => {
     try {
+        // Apply sorting to scores data
+        let sortedScores = scoresData.slice();
+
+        if (sortByName === 'asc') {
+            sortedScores.sort((a, b) => a.user.localeCompare(b.user));
+        } else if (sortByName === 'desc') {
+            sortedScores.sort((a, b) => b.user.localeCompare(a.user));
+        }
+
+        if (sortByScore === 'asc') {
+            sortedScores.sort((a, b) => a.score - b.score);
+        } else if (sortByScore === 'desc') {
+            sortedScores.sort((a, b) => b.score - a.score);
+        }
+
         // Get the scores data for the current page
-        const scoresPerPage = scoresData.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+        const scoresPerPage = sortedScores.slice((currentPage - 1) * pageSize, currentPage * pageSize);
 
         // Get the table body element
-        const tableBody = document.querySelector("tbody");
+        const tableBody = document.querySelector('tbody');
 
         // Clear the table body
-        tableBody.innerHTML = "";
+        tableBody.innerHTML = '';
 
         // Iterate over the scores data and create table rows
         scoresPerPage.forEach((score, index) => {
             // Create a new table row
-            const row = document.createElement("tr");
+            const row = document.createElement('tr');
 
             // Create table cells for the index, name, and score
-            const indexCell = document.createElement("td");
-            const nameCell = document.createElement("td");
-            const scoreCell = document.createElement("td");
+            const indexCell = document.createElement('td');
+            const nameCell = document.createElement('td');
+            const scoreCell = document.createElement('td');
 
             // Set the text content of the cells
             indexCell.textContent = (currentPage - 1) * pageSize + index + 1;
@@ -43,15 +60,15 @@ const refreshScores = () => {
         });
 
         // Update the pagination information
-        const paginationInfo = document.getElementById("pagination-info");
+        const paginationInfo = document.getElementById('pagination-info');
         const startScore = (currentPage - 1) * pageSize + 1;
-        const endScore = Math.min(currentPage * pageSize, scoresData.length);
-        const totalScores = scoresData.length;
-        const totalPages = Math.ceil(scoresData.length / pageSize);
+        const endScore = Math.min(currentPage * pageSize, sortedScores.length);
+        const totalScores = sortedScores.length;
+        const totalPages = Math.ceil(sortedScores.length / pageSize);
 
-        paginationInfo.textContent = `Showing ${startScore} - ${endScore} of ${totalScores} (Page ${currentPage} out of ${totalPages})`;
+        paginationInfo.textContent = `Showing ${startScore} - ${endScore} of ${totalScores} (Page ${currentPage}/${totalPages})`;
     } catch (error) {
-        throw new Error("An error occurred while refreshing scores:", error);
+        throw new Error('An error occurred while refreshing scores:', error);
     }
 };
 
@@ -63,24 +80,27 @@ const fetchScoresData = async () => {
             scoresData = data.result;
             refreshScores();
         } else {
-            throw new Error("Failed to retrieve scores for the game.");
+            throw new Error('Failed to retrieve scores for the game.');
         }
     } catch (error) {
-        throw new Error("An error occurred while fetching scores:", error);
+        throw new Error('An error occurred while fetching scores:', error);
     }
 };
 
-const refreshButton = document.getElementById("refresh");
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
+const refreshButton = document.getElementById('refresh');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const nameSortButton = document.getElementById('sort-name');
+const scoreSortButton = document.getElementById('sort-score');
 
 // Add event listener to the Refresh button click event
-refreshButton.addEventListener("click", () => {
-    refreshScores();
+refreshButton.addEventListener('click', () => {
+    currentPage = 1; // Reset current page to 1
+    location.reload();
 });
 
 // Add event listener to the Prev button click event
-prevButton.addEventListener("click", () => {
+prevButton.addEventListener('click', () => {
     if (currentPage > 1) {
         currentPage--;
         refreshScores();
@@ -88,7 +108,7 @@ prevButton.addEventListener("click", () => {
 });
 
 // Add event listener to the Next button click event
-nextButton.addEventListener("click", () => {
+nextButton.addEventListener('click', () => {
     const totalPages = Math.ceil(scoresData.length / pageSize);
     if (currentPage < totalPages) {
         currentPage++;
@@ -96,9 +116,32 @@ nextButton.addEventListener("click", () => {
     }
 });
 
-// Add event listener to the Refresh On Document load
-document.addEventListener("DOMContentLoaded", () => {
-    fetchScoresData();
+// Add event listener to the Name Sort button click event
+nameSortButton.addEventListener('click', () => {
+    if (sortByName === '') {
+        sortByName = 'asc';
+    } else if (sortByName === 'asc') {
+        sortByName = 'desc';
+    } else if (sortByName === 'desc') {
+        sortByName = '';
+    }
+    refreshScores();
 });
 
+// Add event listener to the Score Sort button click event
+scoreSortButton.addEventListener('click', () => {
+    if (sortByScore === '') {
+        sortByScore = 'asc';
+    } else if (sortByScore === 'asc') {
+        sortByScore = 'desc';
+    } else if (sortByScore === 'desc') {
+        sortByScore = '';
+    }
+    refreshScores();
+});
+
+// Fetch scores data on document load
+document.addEventListener('DOMContentLoaded', fetchScoresData);
+
 export default refreshScores;
+s
